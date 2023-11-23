517e49c00d0f27f74d0aea507cbefb589a951149
taranovskiy <8756136@gmail.com>
2023-11-23 13:59:25 +0300

    implements function invertObj

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,10 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (!obj) {
+    return;
+  }
 
+  const entries = Object.entries(obj);
+  return Object.fromEntries(entries.map(([key, value]) => [value, key]));
 }
