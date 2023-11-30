40cf7918e829b78eeec124fa637ea520858e70f9
taranovskiy <8756136@gmail.com>
2023-11-30 18:56:26 +0300

    implements sorting on click

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,24 @@
-export default class SortableTable {
-  constructor(headersConfig, {
+import SortableTableV1 from '../../05-dom-document-loading/2-sortable-table-v1/index.js';
+
+export default class SortableTable extends SortableTableV1 {
+  constructor(headerConfig, {
     data = [],
     sorted = {}
   } = {}) {
+    super(headerConfig, data);
 
+    this.sorted = sorted;
+
+    this.addHeaderEventListeners();
+  }
+
+  handleHeaderClick = ({ target }) => {
+    const { id } = target.closest('[data-id]').dataset;
+    this.sort(id);
+  }
+
+  addHeaderEventListeners() {
+    const { header } = this.subElements;
+    header.addEventListener('click', this.handleHeaderClick);
   }
 }
