82927a897ba2e0bd002cef70d4e92b9eca9a8fcf
taranovskiy <8756136@gmail.com>
2023-12-01 16:26:57 +0300

    implements SortableTable V2 component

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,24 +1,65 @@
-import SortableTableV1 from '../../05-dom-document-loading/2-sortable-table-v1/index.js';
+import SortableTableBase from '../../05-dom-document-loading/2-sortable-table-v1/index.js';
 
-export default class SortableTable extends SortableTableV1 {
+export default class SortableTable extends SortableTableBase {
   constructor(headerConfig, {
     data = [],
     sorted = {}
   } = {}) {
-    super(headerConfig, data);
+    const sortedData = sorted.id ? SortableTableBase.sortData(headerConfig, data, sorted.id, sorted.order) : data;
 
+    super(headerConfig, sortedData);
     this.sorted = sorted;
 
-    this.addHeaderEventListeners();
+    this.updateHeaderCellsDatasetOrder();
+    this.addEventListeners();
+  }
+
+  updateHeaderCellsDatasetOrder() {
+    const { header } = this.subElements;
+    header.querySelectorAll('[data-id]').forEach(({dataset}) => {
+      if (dataset.id === this.sorted.id && dataset.sortable === 'true') {
+        dataset.order = this.sorted.order;
+      } else {
+        delete dataset.order;
+      }
+    });
+  }
+
+  updateSortedId(id) {
+    this.sorted.id = id;
+  }
+
+  toggleSortedOrder() {
+    this.sorted.order = this.sorted.order === 'asc' ? 'desc' : 'asc';
+  }
+
+  sort(id) {
+    this.updateSortedId(id);
+    this.toggleSortedOrder();
+    super.sort(id, this.sorted.order);
+    this.updateHeaderCellsDatasetOrder();
   }
 
   handleHeaderClick = ({ target }) => {
-    const { id } = target.closest('[data-id]').dataset;
+    const { id, sortable } = target.closest('[data-id]').dataset;
+    if (sortable === 'false') {
+      return;
+    }
     this.sort(id);
   }
 
-  addHeaderEventListeners() {
+  addEventListeners() {
     const { header } = this.subElements;
-    header.addEventListener('click', this.handleHeaderClick);
+    header.addEventListener('pointerdown', this.handleHeaderClick);
+  }
+
+  removeEventListeners() {
+    const { header } = this.subElements;
+    header.removeEventListener('pointerdown', this.handleHeaderClick);
+  }
+
+  destroy() {
+    super.destroy();
+    this.removeEventListeners();
   }
 }
